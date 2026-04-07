0b0dd7cc123a125567f936db194d3b75ce301b22
dhadhalhardi2004-droid <dhadhalhardi2004@gmail.com>
2026-04-07 17:27:10 +0000

    updated code

--- chiggy_wiggy/Frontend/src/App.jsx
+++ chiggy_wiggy/Frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-
+import{Route,Routes} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 const App = () => {
   return (
     <div className='app'>
+
+      
       <Navbar />
+    <Routes>
+      <Route path='/' element={<Home/>}>
+         <Route path='/Cart' element={<Cart/>}></Route>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/PlaceOrder' element={<PlaceOrder/>}></Route>
+      </Route>
+    </Routes>
     </div>
   )
 }
